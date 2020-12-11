5215f9f9c4f1a93d865094e50d7ce60cca48d250
Yash254 <72601812+Yash254@users.noreply.github.com>
2020-12-11 04:20:01 -0500

    Add files via upload

---
+++ Food.js
@@ -0,0 +1,7 @@
+class Form {
+    constructor(){
+        var foodStock;
+        var lastFed;
+        
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,50 @@
+var dogImg,dog1Img;
+var database;
+var foodS,foodStock;
+
+function preload()
+{
+  dogImg = loadImage("images/dogImg.png");
+  dog1Img = loadImage("images/dogImg1.png");
+}
+
+function setup() {
+  createCanvas(500, 500);
+  
+  database=firebase.database();
+  dog = createSprite(250,300,150,150);
+  dog.addImage(dogImg);
+  dog.scale=0.2;
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock);
+
+}
+
+function draw() {  
+background(46,139,87);
+
+  if (keyWentDown(UP_ARROW)) {
+  writeStock(foodS);
+  dog.addImage(dog1Img)
+}
+  drawSprites();
+  stroke("black"); 
+  text("Food remaining : "+foodS,170,200);
+   textSize(13); 
+   text("Note: Press UP_ARROW Key To Feed Drago Milk!",130,10,300,20);
+  }
+
+function readStock(data) {
+  foodS=data.val();
+}
+
+function writeStock(x) {
+  if (x<=0) {
+    x=0;
+  } else {
+    x=x-1;
+  }
+  database.ref('/').update({
+    Food:x
+  })
+}
